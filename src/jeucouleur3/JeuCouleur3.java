9455f77e034e719f7b846ab2d8afc3c18bc402bf
scartoning2020 <35373637+scartoning2020@users.noreply.github.com>
2018-02-10 16:28:45 +0100

    hein

--- src/jeucouleur3/JeuCouleur3.java
+++ src/jeucouleur3/JeuCouleur3.java
@@ -15,16 +15,7 @@ public class JeuCouleur3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-        System.out.println("Is this the real life ?");
-        System.out.println("Is this just fantasy ?");
-        System.out.println("Caught in the land slide ...");
-        System.out.println("C'est pas vrai !");
-        
         Jeu game = new Jeu();
-        //while ( !game.isJoueursInitialized() ){}
-        //game.start();
     }
     
 }
